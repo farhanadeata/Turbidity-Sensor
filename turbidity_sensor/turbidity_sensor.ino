9a9a956197bbdc0aa53c17d9fffc7eef8ee90d59
farhanadeata <fadeatalarik@gmail.com>
2025-01-27 05:49:23 +0700

    FULL CODE CONTACT ME

--- turbidity_sensor/turbidity_sensor.ino
+++ turbidity_sensor/turbidity_sensor.ino
@@ -6,7 +6,6 @@ void setup()
 {
   lcd.begin(16,2);
   pinMode(3,OUTPUT);
-  pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
 
 }
@@ -22,25 +21,12 @@ void loop() {
   lcd.print(turbidity);
   delay(100);
   if (turbidity < 50) {
-    digitalWrite(7, HIGH);
-    digitalWrite(8, LOW);
-    digitalWrite(9, LOW);
-    lcd.setCursor(0, 1);
     lcd.print(" bersih");
   }
-  if ((turbidity > 40) && (turbidity < 50)) {
-    digitalWrite(7, LOW);
-    digitalWrite(8, HIGH);
-    digitalWrite(9, LOW);
-    lcd.setCursor(0, 1);
-    lcd.print(" keruh ");
-  }
-  if (turbidity > 30) {
-    digitalWrite(7, LOW);
-    digitalWrite(8, LOW);
-    digitalWrite(9, HIGH);
-    lcd.setCursor(0, 1);
-    lcd.print(" kotor ");
-  }
-
+  
 }
+
+//full code please Contact me
+//fadeatalarik@gmail.com
+//farhanadeata (IG)
+
